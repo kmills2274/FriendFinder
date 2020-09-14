e4bb80cc852f3f1702a7a210a97cb87d059382df
kate.m <katiemills0810@gmail.com>
2020-09-13 19:32:02 -0500

    friends data added

---
+++ app/data/friends.js
@@ -0,0 +1,167 @@
+// Variable to hold the array of "friends" data
+
+var friendsArray = [
+    {
+      friendName: "Carlos",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a06605b1e4a_-_cos-dream-phone-carlos-17053563.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          2,
+          4,
+          3,
+          1,
+          2,
+          1,
+          1,
+          1
+        ]
+    },
+    {
+      friendName: "Dale",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065eea6cee_-_cos-dream-phone-dale-77158859.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          4,
+          2,
+          5,
+          1,
+          3,
+          5,
+          4,
+          1
+        ]
+    },
+    {
+      friendName: "George",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065fe96e1a_-_cos-dream-phone-george-32824843.jpg?resize=768:*",
+      "scores[]":[
+          3,
+          1,
+          4,
+          3,
+          5,
+          1,
+          2,
+          4,
+          2,
+          1
+        ]
+    }, 
+    {
+      friendName: "James",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a06601c0d92_-_cos-dream-phone-james-20896596.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          1,
+          3,
+          5,
+          5,
+          2,
+          4,
+          5,
+          5
+        ]
+    }, 
+    {
+      friendName: "Jason",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065ebc4dcd_-_cos-dream-phone-jason-41416121.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          4,
+          2,
+          2,
+          1,
+          4,
+          5,
+          4,
+          2
+        ]
+    }, 
+    {
+      friendName: "John",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065f63bbaa_-_cos-dream-phone-john-52975506.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          4,
+          4,
+          3,
+          3,
+          1,
+          2,
+          3,
+          4,
+          2
+        ]
+    }, 
+    {
+      friendName: "Matt",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a06607ae947_-_cos-dream-phone-matt-54297305.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          5,
+          4,
+          2,
+          5,
+          3,
+          2,
+          3,
+          3,
+          1
+        ]
+    }, 
+    {
+      friendName: "Phil",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065f79d0fd_-_cos-dream-phone-phil-51112179.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+    }, 
+    {
+      friendName: "Spencer",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065ec7d102_-_cos-dream-phone-spencer-62838041.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          4,
+          2,
+          5,
+          4,
+          2,
+          3,
+          4,
+          2
+        ]
+    }, 
+    {
+      friendName: "Tony",
+      "photo":"https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a065ead1ec8_-_cos-dream-phone-tony-36309440.jpg?resize=768:*",
+      "scores[]":[
+          5,
+          1,
+          4,
+          3,
+          5,
+          5,
+          4,
+          5,
+          5,
+          1
+        ]
+    }
+  ];
+  
+// Export the data
+  module.exports = friendsArray;
