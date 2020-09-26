9645b19820c50be956b99c04970a2aabafbd468a
kate.m <katiemills0810@gmail.com>
2020-09-25 20:11:59 -0500

    changed port on server.js

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 var express = require("express");
 var path = require("path");
 
-
 var app = express();
-var PORT =process.env.PORT || 3000;
+var PORT =process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
+// Routing for app
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
+// Port listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
